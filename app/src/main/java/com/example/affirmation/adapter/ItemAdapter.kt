7727b471675c274541a3e8e93d82bafd833f41e7
lenovo <suryahadikusuma351@gmail.com>
2022-09-30 14:37:28 +0700

    sd

---
+++ app/src/main/java/com/example/affirmation/adapter/ItemAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.affirmation.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmation.R
+import com.example.affirmation.model.Affirmation
+
+/**
+ * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
+ */
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    /*memberikan referensi untuk tampilan setiap item data
+    Item data yang kompleks bisa memerlukan lebih dari satu tampilan per item dan
+    memberikan akses ke semua tampilan untuk item data dalam penampung tampilan.
+    dan Setiap item data hanyalah objek Afirmasi*/
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    /**
+     * Create new views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
+    override fun getItemCount() = dataset.size
+}
